53f04aa0083066d3ec0975415c58ec997e99b919
Causaelity <Tiberius@Faith.local>
2018-02-10 12:44:33 -0800

    Code Review Comments

--- app/src/main/java/com/example/android/scorekeeperbasketball/Team.java
+++ app/src/main/java/com/example/android/scorekeeperbasketball/Team.java
@@ -9,80 +9,96 @@ import android.widget.TextView;
 
 public class Team {
 
-    private int Score;
-    private String Name;
+    // variable names should be lowercase
+    // Uppercase is typically reserved for things like classes.
+    private int score;
+    private String name;
 
-    private TextView ScoreDisplay;
-    private TextView NameDisplay;
+    private TextView scoreDisplay;
+    private TextView nameDisplay;
+
+    // setup scoring
+    final int THREEPOINTS = 3;
+    final int BASKET = 2;
+    final int FREETHROW =1 ;
+
+
+
 
     //Constructor
     public Team (int score, String name, TextView displayScore, TextView displayName) {
-        Score = score;
-        Name = name;
-        ScoreDisplay = displayScore;
-        NameDisplay = displayName;
+        // when you use an external variable that uses the same name as an internal variable
+        // you've created, you can use "this" to denote the internal one so you can assign
+        // the external to the internal
+        this.score = score;
+        this.name = name;
+        scoreDisplay = displayScore;
+        nameDisplay = displayName;
     }
 
     //mutator
     public void setName(String name){
-        Name = name;
+        this.name = name;
     }
 
     //accessor
     public String getName() {
-        return Name;
+        return this.name;
     }
 
     //mutator
     public void setScoreDisplay(TextView view){
-        ScoreDisplay = view;
+        scoreDisplay = view;
     }
 
     //accessor
     public TextView getScoreDisplay(){
-        return ScoreDisplay;
+        return scoreDisplay;
     }
 
     //mutator
     public void setNameDisplay(TextView view){
-        NameDisplay = view;
+        nameDisplay = view;
     }
 
     //accessor
     public TextView getNameDisplay(){
-        return NameDisplay;
+        return nameDisplay;
     }
 
     //mutator
     public void setScore(int score){
-        Score = score;
+        this.score = score;
     }
 
     public int getScore(){
-        return Score;
+        return this.score;
     }
 
     public void displayName(String name){
-        NameDisplay.setText(name);
+        nameDisplay.setText(name);
     }
 
     public void displayScore(int score) {
-        ScoreDisplay.setText(String.valueOf(score));
+        scoreDisplay.setText(String.valueOf(score));
     }
 
+    // You should really consider creating constants for your scores opposed to using
+    // "magic" numbers
+
     public void threePoints(View view) {
-        Score += 3;
-        displayScore(Score);
+        score += THREEPOINTS;
+        displayScore(score);
     }
 
     public void basket(View view) {
-        Score += 2;
-        displayScore(Score);
+        score += BASKET;
+        displayScore(score);
     }
 
     public void freeThrow(View view) {
-        Score += 1;
-        displayScore(Score);
+        score += FREETHROW;
+        displayScore(score);
     }
 
 }
